40185ab6a122f91b029ff0eeff294395458326e2
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-01 18:49:03 -0400

    Implement routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Navbar from '../Navbar/Navbar';
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<p>Confirm</p>}/>
+      </Routes>
+    </>
   )
 }
 
