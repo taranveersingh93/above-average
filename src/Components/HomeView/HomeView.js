d85718a2c4f882d568aa3cb427d062d4c2896513
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-01 22:11:10 -0400

    Make extract data function

--- src/Components/HomeView/HomeView.js
+++ src/Components/HomeView/HomeView.js
@@ -4,28 +4,24 @@ import SpButton from './SpButton/SpButton';
 import { Link } from 'react-router-dom';
 import { fetchSp500 } from '../../apiCalls';
 import { useEffect, useState } from 'react';
+import { extractData } from '../../helperFunctions';
 
 const HomeView = () => {
   const [spData, setSpData] = useState({});
 
-  const extractData = (data) => {
-    const baseData = data.historical.slice(0,150);
-    const allClosingPrices = baseData.map(day => day.close);
-    const priceSum = allClosingPrices.reduce((sum, price) => sum + price, 0);
-    const average = (priceSum/150).toFixed(2);
-
-  }
-
   useEffect(() => {
     fetchSp500().then(data => {
-      extractData(data)
+      extractData(data, setSpData)
     })
   }, [])
+
+  
+
   return (
     <div className='home-view'>
       <IntroText />
       <Link to='/sp500'>
-        {/* <SpButton currentPrice={currentPrice} previousPrice={previousPrice} average={average}/> */}
+        {/* <SpButton currentPrice={currentPrice} average={average}/> */}
       </Link>
     </div>
   )
---
+++ src/helperFunctions.js
@@ -0,0 +1,19 @@
+const extractData = (data, setterFn) => {
+  const baseData = data.historical.slice(0,150);
+  const allClosingPrices = baseData.map(day => day.close.toFixed(2));
+  const initialClose = allClosingPrices[149];
+  const lastClose = allClosingPrices[0];
+  const longTermReturn = (((lastClose - initialClose)*100)/initialClose).toFixed(2);
+  const priceSum = allClosingPrices.reduce((sum, price) => sum + price, 0);
+  const average = (priceSum/150).toFixed(2);
+  const changePercent = baseData[0].changePercent;
+  const extraction = {
+    lastClose: lastClose,
+    changePercent: changePercent,
+    average: average,
+    longTermReturn: longTermReturn
+  }
+  setterFn(extraction);
+}
+
+export {extractData}
