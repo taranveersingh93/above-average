f766485dc34638e0c3e7069869572d7b1455663a
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-02 20:39:43 -0400

    Make stocks view responsive

--- src/Components/StockCard/StockCard.js
+++ src/Components/StockCard/StockCard.js
@@ -3,14 +3,27 @@ import './StockCard.css';
 const StockCard = ({symbol, name, data, id}) => {
   return (
     <div className='stock-card' id={id}>
-      <p>Rank: {id}</p>
-      <div className='name-symbol'>
-        <h3>{symbol}</h3>
-        <p>{name}</p>
+      <div className='stock-info'>
+        <p>Rank</p>
+        <h3>{id}</h3>
       </div>
-      <p>Last Close: {data.lastClose}</p>
-      <p>Change: {data.changePercent}</p>
-      <p>150d Return: {data.longTermReturn}%</p>
+      <div className='stock-info'>
+        <p>{name}</p>
+        <h3>{symbol}</h3>
+      </div>
+      <div className='stock-info'>
+        <p>Last Close</p>
+        <h3>{data.lastClose}</h3>
+      </div>
+      <div className='stock-info'>
+        <p>Change</p>
+        <h3>{data.changePercent}</h3>
+      </div>
+      <div className='stock-info'>
+        <p>150d Return</p>
+        <h3>{data.longTermReturn}%</h3>
+      </div>
+      
     </div>
   )
 }
