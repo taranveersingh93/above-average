c86c7daa8f1226749404b2923ea859a3413b1fc1
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-03 16:25:57 -0400

    Add error handling in watchlist

--- src/Components/Watchlist/Watchlist.js
+++ src/Components/Watchlist/Watchlist.js
@@ -4,17 +4,25 @@ import { useEffect, useState } from 'react';
 
 const Watchlist = ({savedConstituents, toggleStockFromWatchlist}) => {
   const [stocksCode, setStocksCode] = useState([]);
+  const [stocksSaved, setStocksSaved] = useState(false);
 
   useEffect(() => {
     const watchlistCode = makeStockCards(savedConstituents, toggleStockFromWatchlist);
-    setStocksCode(watchlistCode);
+    setStocksCode(watchlistCode);  
+
+    if (savedConstituents.length) {
+      setStocksSaved(true);
+    } else {
+      setStocksSaved(false);
+    }
   }, [savedConstituents])
 
   return (
     <div className="watchlist-view">
-      <h2 className="heading">Displaying {savedConstituents.length} stocks that are above their 150 Day Moving Average</h2>
-      <p className="subheading">These stocks are ranked by their 150 Day return.</p>
-      {stocksCode}
+      {stocksSaved && <h2 className="heading">Displaying {savedConstituents.length} stocks that are above their 150 Day Moving Average</h2>}
+      {stocksSaved && <p className="subheading">These stocks are ranked by their 150 Day return.</p>}
+      {stocksSaved && stocksCode}
+      {!stocksSaved && <h2>You have no stocks saved</h2>}
     </div>
   )
 }
