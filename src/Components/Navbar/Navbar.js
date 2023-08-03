8214f1ff558189d2bd6356068c76dd2ecd131116
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-03 14:31:48 -0400

    Add navlinks

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,15 @@
 import './Navbar.css'
 import Logo from './Logo/Logo';
+import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className='navbar'>
       <Logo />
+      <div className='navlinks'>
+        <NavLink to='/'><p>Home</p></NavLink>
+        <NavLink to='/stocksView'><p>Nasdaq Constituents</p></NavLink>
+        <NavLink to='/watchlist'><p>Watchlist</p></NavLink>
+      </div>
     </div>
   )
 }
