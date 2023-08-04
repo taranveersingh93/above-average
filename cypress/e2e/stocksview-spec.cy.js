6f2d2db6fa03806143c1735a753256570120843f
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-04 17:26:20 -0400

    Make tests for stocksview page

--- cypress/e2e/stocksview-spec.cy.js
+++ cypress/e2e/stocksview-spec.cy.js
@@ -1,29 +1,42 @@
 import nasdaqConstituentsStub from "../fixtures/nasdaqConstituents"
+
 const allSymbols = ["AAPL", "ABNB", "ADBE", "ADI", "ADP"];
 
 describe('Stocksview spec', () => {
-  it('shows the right elements', () => {
-    // cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/%5ENDX?apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
-    //   statusCode: 200,
-    //   fixture: 'nasdaqResponse'
-    // }).as('getNasdaqData')
-
+  beforeEach(() => {
     cy.intercept('GET', `https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
-      statusCode: 200,
-      body: nasdaqConstituentsStub
-    }).as('getNasdaqConstituents')
+     statusCode: 200,
+     body: nasdaqConstituentsStub
+   }).as('getNasdaqConstituents')
+  
+   allSymbols.forEach(symbol => {
+     cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?timeseries=151&apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
+       statusCode: 200,
+       fixture: `stub${symbol}`
+     }).as(`get${symbol}`)
+   })
+  
+   cy.visit('localhost:3000/stocksView')
+   cy.wait('@getNasdaqConstituents')
+   const symbolWaitArray = allSymbols.map(symbol => `@get${symbol}`)
+   cy.wait(symbolWaitArray);
+  
+  })
 
-    allSymbols.forEach(symbol => {
-      cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?timeseries=151&apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
-        statusCode: 200,
-        fixture: `stub${symbol}`
-      }).as(`get${symbol}`)
-    })
-
-    cy.visit('localhost:3000/stocksView')
-    cy.wait('@getNasdaqConstituents')
-    const symbolWaitArray = allSymbols.map(symbol => `@get${symbol}`)
-    cy.wait(symbolWaitArray);
-
+  it('should show the right elements', () => {
+    cy.get('.searchbar')
+    cy.get('.heading').contains('Displaying 5 stocks that are above their 150 Day Moving Average')
+    cy.get('.subheading').contains('These stocks are ranked by their 150 Day return')
+    cy.get('.stock-card').should('have.length', 5)
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(0).find('p').contains('Rank')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(0).find('h3').contains('2')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(1).find('p').contains('Adobe Inc.')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(1).find('h3').contains('ADBE')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(2).find('p').contains('Last Close')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(2).find('h3').contains('526.88')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(3).find('p').contains('Last Day Change')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(3).find('h3').contains('0.03%')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(4).find('p').contains('150d Return')
+    cy.get('.stock-card').eq(1).find('.stock-info').eq(4).find('h3').contains('56.08%')
   })
 })
--- cypress/fixtures/nasdaqConstituents.js
+++ cypress/fixtures/nasdaqConstituents.js
@@ -51,4 +51,6 @@ const nasdaqConstituentsStub = [
   }
 ]
 
+
+
 export default nasdaqConstituentsStub;
