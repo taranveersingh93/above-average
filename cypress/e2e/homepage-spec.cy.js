5109639da97b9f1f5230013fc4650318b121bcd3
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-04 14:36:38 -0400

    Test landing page

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -6,8 +6,15 @@ describe('template spec', () => {
     }).as('getNasdaqData')
     cy.visit('localhost:3000')
     cy.wait('@getNasdaqData')
-
   })
-  it('passes', () => {
+  it('shows the right elements', () => {
+    cy.get('.intro-text-container').contains('Now you can easily track')
+    cy.get('.index-container').children().should('have.length', 5);
+    cy.get('.index-container').find('p').eq(0).contains('(Click to view Nasdaq constituents)');
+    cy.get('.index-container').find('p').eq(1).contains('Last close: 15457.81');
+    cy.get('.index-container').find('p').eq(2).contains('Moving average: 13335.12');
+    cy.get('.index-container').find('p').eq(3).contains('NDX 100 is above its moving average');;
+    cy.get('.index-container').find('h2').contains('NDX 100');
+    cy.get('.navlinks').find('.active').contains('Home');
   })
 })
