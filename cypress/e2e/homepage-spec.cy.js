f464302c300f516070b7317eea88960d62425652
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-04 14:12:30 -0400

    Import env variables

--- cypress/e2e/homepage-spec.cy.js
+++ cypress/e2e/homepage-spec.cy.js
@@ -1,5 +1,13 @@
 describe('template spec', () => {
+  beforeEach(() => {
+    cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/%5ENDX?apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
+      statusCode: 200,
+      fixture: 'nasdaqResponse'
+    }).as('getNasdaqData')
+    cy.visit('localhost:3000')
+    cy.wait('@getNasdaqData')
+
+  })
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
   })
 })
