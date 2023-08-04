1832836105fe5475e54179b5959c4f507598f30c
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-04 16:30:26 -0400

    Stub stock fetches

--- cypress/e2e/stocksview-spec.cy.js
+++ cypress/e2e/stocksview-spec.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import nasdaqConstituentsStub from "../fixtures/nasdaqConstituents"
+const allSymbols = ["AAPL", "ABNB", "ADBE", "ADI", "ADP"];
+
+describe('Stocksview spec', () => {
+  it('shows the right elements', () => {
+    // cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/%5ENDX?apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
+    //   statusCode: 200,
+    //   fixture: 'nasdaqResponse'
+    // }).as('getNasdaqData')
+
+    cy.intercept('GET', `https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
+      statusCode: 200,
+      body: nasdaqConstituentsStub
+    }).as('getNasdaqConstituents')
+
+    allSymbols.forEach(symbol => {
+      cy.intercept('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?timeseries=151&apikey=${Cypress.env(`REACT_APP_API_KEY`)}`, {
+        statusCode: 200,
+        fixture: `stub${symbol}`
+      }).as(`get${symbol}`)
+    })
+
+    cy.visit('localhost:3000/stocksView')
+    cy.wait('@getNasdaqConstituents')
+    const symbolWaitArray = allSymbols.map(symbol => `@get${symbol}`)
+    cy.wait(symbolWaitArray);
+
   })
 })
---
+++ cypress/fixtures/nasdaqConstituents.js
@@ -0,0 +1,54 @@
+const nasdaqConstituentsStub = [
+  {
+      "symbol": "AAPL",
+      "name": "Apple Inc.",
+      "sector": "Technology",
+      "subSector": "Technology",
+      "headQuarter": "Cupertino, CA",
+      "dateFirstAdded": null,
+      "cik": "0000320193",
+      "founded": "1976-04-01"
+  },
+  {
+      "symbol": "ABNB",
+      "name": "Airbnb",
+      "sector": "Consumer Cyclical",
+      "subSector": "Consumer Cyclical",
+      "headQuarter": "San Francisco, CA",
+      "dateFirstAdded": null,
+      "cik": "0001559720",
+      "founded": "2008-08-01"
+  },
+  {
+      "symbol": "ADBE",
+      "name": "Adobe Inc.",
+      "sector": "Technology",
+      "subSector": "Technology",
+      "headQuarter": "San Jose, CA",
+      "dateFirstAdded": null,
+      "cik": "0000796343",
+      "founded": "1982-12-01"
+  },
+  {
+      "symbol": "ADI",
+      "name": "Analog Devices",
+      "sector": "Technology",
+      "subSector": "Technology",
+      "headQuarter": "Wilmington, MA",
+      "dateFirstAdded": null,
+      "cik": "0000006281",
+      "founded": "1965-01-01"
+  },
+  {
+      "symbol": "ADP",
+      "name": "ADP",
+      "sector": "Industrials",
+      "subSector": "Industrials",
+      "headQuarter": "Roseland, NJ",
+      "dateFirstAdded": null,
+      "cik": "0000008670",
+      "founded": "1949-01-01"
+  }
+]
+
+export default nasdaqConstituentsStub;
