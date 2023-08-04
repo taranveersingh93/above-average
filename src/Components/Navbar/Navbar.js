1681aaa6100066a165a28ae30c8ff36e6e092247
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-04 01:29:45 -0400

    Add sticky navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import './Navbar.css'
 import Logo from './Logo/Logo';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  
+  const handleScroll = () => {
+    if (window.scrollY > 150) {
+      setScrolled(true)
+    } else {
+      setScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className='navbar'>
+    <div className={`${scrolled? "navbar sticky": "navbar"}`}>
       <Logo />
       <div className='navlinks'>
         <NavLink to='/'><p>Home</p></NavLink>
