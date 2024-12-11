86ea34cd693786dfb157dc18e6b445474be14a03
Taranveer Singh <taranveersingh93@gmail.com>
2024-12-10 19:32:58 -0500

    Remove duplicate rank

--- src/Components/StockCard/CardComponents/TopSection.js
+++ src/Components/StockCard/CardComponents/TopSection.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 const TopSection = ({id, name, data}) => {
     return (
         <div className="d-flex justify-content-around p-3 align-items-center">
-          <p className="rank-text fs-3">
-            #{id}
-          </p>
+
           <div className="d-flex flex-column align-items-center">
-            <h5 className="font-weight-300 text-center">#{id} {name}</h5>
+            <h5 className="font-weight-300">
+              <span className="small-text">
+                #{id}&nbsp;&nbsp;&nbsp;
+              </span> 
+              {name}
+            </h5>
             <p className="font-weight-300 small-text text-center">Last Close: {data?.lastClose}</p>
           </div>
         </div>
